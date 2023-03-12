257503f125881688183fe662d396eae2733fb112
dmtrmkhlv <app@dmtrmkhlv.ru>
2023-03-12 17:35:21 +0300

    Вынесите код переключения между разделами (таймер/калькулятор дат) в отдельный модуль.

--- lesson-1/dateDiff/js/main.js
+++ lesson-1/dateDiff/js/main.js
@@ -1,21 +1,29 @@
-import { printError, printREsult } from './printResult.js'
-import getDateDiff from './getDateDiff.js'
+import { printError, printREsult } from "./printResult.js";
+import getDateDiff from "./getDateDiff.js";
+import setSection from "./setSection.js";
 
-const form = document.getElementById('datecalc')
-
+const form = document.getElementById("datecalc");
 
 form.onsubmit = (event) => {
   event.preventDefault();
 
-  const formData = new FormData(event.target)
+  const formData = new FormData(event.target);
 
-  const firsDate = formData.get('firstDate')
-  const secondDate = formData.get('secondDate')
+  const firsDate = formData.get("firstDate");
+  const secondDate = formData.get("secondDate");
 
   if (!firsDate || !secondDate) {
-    printError('Oooooopppsss - введите текст!!!!')
+    printError("Oooooopppsss - введите текст!!!!");
   } else {
-    const dateDiff = getDateDiff(firsDate, secondDate)
-    printREsult(dateDiff)
+    const dateDiff = getDateDiff(firsDate, secondDate);
+    printREsult(dateDiff);
   }
-}
+};
+
+console.log(document.querySelectorAll(".button"));
+const buttons = document.querySelectorAll(".button");
+[...buttons].forEach((button) => {
+  button.addEventListener("click", () => {
+    setSection(button.dataset.section, "section");
+  });
+});
